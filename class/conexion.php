42e2ea85bd506b75a4badf5001e0f1fb4a4bdbfa
Alvaro Pacheco <sistemas.pacheco@gmail.com>
2022-07-12 12:19:06 -0400

    Update conexion.php

--- class/conexion.php
+++ class/conexion.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/La_Paz');
 setlocale(LC_CTYPE, "es_ES");
 setlocale(LC_ALL, 'esp');
-$host="localhost";
-$user="pacheco";
+$host="137.184.126.52";
+$user="root";
 $pass="DaniloMoreno4";
 $database="db_invoice";
